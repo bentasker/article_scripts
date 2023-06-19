845bb54232df7dfb181d12d8d8decbc2115107dd
B Tasker <github@bentasker.co.uk>
2023-06-19 11:48:11 +0100

    Fix key.
    Old one has also been invalidated

--- interacting-with-influxdb-from-an-arduino-compatible-opensource-watch/7_SEG/settings.h
+++ interacting-with-influxdb-from-an-arduino-compatible-opensource-watch/7_SEG/settings.h
@@ -3,7 +3,7 @@
 
 //Weather Settings
 #define CITY_ID "2646057" //New York City https://openweathermap.org/current#cityid
-#define OPENWEATHERMAP_APIKEY "b458b37ac8361ec27140f1fd09948e08" //use your own API key :)
+#define OPENWEATHERMAP_APIKEY "f058fe1cad2afe8e2ddc5d063a64cecb" //use your own API key :)
 #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
 #define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
